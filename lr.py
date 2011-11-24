0e239bef1f4a708f48fdfb5273a8832012ec889c
osdf <osendorf@gmail.com>
2011-11-24 01:12:55 +0100

    Finished robust bayesian liear regression for batch data.

--- lr.py
+++ lr.py
@@ -36,88 +36,99 @@ def mle_fast(X, t):
     return la.lstsq(X, t)[0]
 
 
-def bayesian(X, t, sigma, mean_0, prec_0, **params):
+def bayesian(X, t, var, mean_0, prec_0, **params):
     """
 
     """
     result = {}
     prec_chol = la.cholesky(prec_0)
     # build up augmented designmatrix/targetvector
-    Xt = np.append(X/sigma, prec_chol, axis=0)
-    tt = np.append(t/sigma, np.dot(prec_chol, mean_0), axis=0)
+    Xt = np.append(X/np.sqrt(var), prec_chol, axis=0)
+    tt = np.append(t/np.sqrt(var), np.dot(prec_chol, mean_0), axis=0)
     # gaussian posterior ...
     result["mean"] = la.lstsq(Xt, tt)[0]
     _, r = la.qr(Xt, mode='economic')
     result["sqrt_prec"] = r
     result["prec"] = np.dot(r.T, r)
+    result["var"] = var
     return result 
 
 
-def predictive(samples, sigma, mean, sqrt_prec, **params):
+def predictive(samples, var, mean, sqrt_prec, **params):
     """
     """
+    result = {}
     # sqrt_eqk -- square root of equivalent kernel, 
     # compare to Bishop, p. 159
     sqrt_eqk = la.solve_triangular(sqrt_prec, samples.T, trans=1).T
-    result = {}
     result["mean"] = np.dot(samples, mean)
-    result["var"] = sigma + np.sum(sqrt_eqk**2, axis=1)
+    result["var"] = var + np.sum(sqrt_eqk**2, axis=1)
     return result 
 
 
-def robust(X, t, mean_0, prec_0, a, b, var=1., **params):
+def robust(X, t, mean_0, prec_0, 
+        a, b, var, eps=10**-4, **params):
     """
     """
     prec_chol = la.cholesky(prec_0)
     Xt = np.append(X, prec_chol, axis=0)
     tt = np.append(t, np.dot(prec_chol, mean_0), axis=0)
     n, d = Xt.shape
-    w = np.ones(n,1)
-    w[:-d] = a/(b*var)
+    w = var*np.ones((n,1))
+    w[:-d] = a/b
     # a only serves as shifted constant
     a += 0.5
     result = {}
     ll_old = -np.inf
+    lls = []
     while True:
         # estimate gaussian for mean
-        mean = la.lstsq(np.sqrt(w)*Xt, np.sqrt(w)*tt)[0]
         q, r = la.qr(np.sqrt(w/var)*Xt, mode='economic')
         del q
-        # estimate gamma 
+        mean = la.lstsq(np.sqrt(w/var)*Xt, np.sqrt(w/var)*tt)[0]
+        # estimate posterior weights 
         # se -- squared error between prediction and target
         se = (t - np.dot(X, mean))**2
         # sqrt_eqk -- square root of equivalent kernel
         sqrt_eqk = la.solve_triangular(r, X.T, trans=1).T
-        mhlb = np.sum(sqrt_eqk**2, axis=1)
-        # check here: officially, first compute w_new
-        var = np.mean(se) + np.mean(mhlb)
+        mhlb = np.sum(sqrt_eqk**2, axis=1)[:, np.newaxis]
         # a stays constant, according to formula in paper
         b_new = b + (se + mhlb)/(2*var)
         w_new = a/b_new
-        w[:-d] = w_new/var
+        # M-Step, estimate sigma
+        var = np.mean(w_new * (se + mhlb))
+        w[:-d] = w_new
+        w[-d:] = var
         # maximize loglikelihood -> track its progress
         # leaving out constant terms
-        ll = -n * np.ln(var)/2
-        ll -= 0.5 * np.sum(w[:-d] * se)
+        ll = np.log(var)/2
+        ll -= np.sum(w[:-d] * (se + mhlb))/(2*var)
         # entropies
         # gaussian of mean, no constant terms
-        ll += np.sum(np.diag(r))
-        ll -= 2 * np.sum(np.ln(b_new))
-        if ((ll - ll_old) < 0.1):
+        ll += (d-n) * np.sum(np.diag(r))
+        # gamma dist, again no constants
+        ll += 2 * np.sum(np.log(b_new))
+        if (ll < ll_old):
+            print "Loglikelihood decreased to %f!" % ll
+            print "This can happen after the first iteration!"
+            lls.append(ll)
+            ll_old = ll
+        elif ((ll - ll_old) < eps):
             result["mean"] = mean
             result["sqrt_prec"] = r
             result["weights"] = w[:-d]
             result["var"] = var
+            result["logl"] = lls
             break
-        if (ll < ll_old):
-            print "STRESS"
         else:
+            print "LogL: ", ll
+            lls.append(ll)
             ll_old = ll
     a -= 0.5
     return result
 
 
-def demo_bayesian(nos, nob):
+def demo_bayesian(nos, nob, var=0.1, width=0.4):
     samples = np.random.rand(nos, 1)
     centers = np.linspace(0, 1, nob)
     width = 4./10
@@ -126,38 +137,86 @@ def demo_bayesian(nos, nob):
         X[i, 0] = 1
         for j, c in enumerate(centers):
             X[i, 1+j] = np.exp(-0.5 * ((s-c)/width)**2)
-    y = np.sin(2*np.pi*samples) + 0.1*np.random.randn(nos, 1)
+    y = np.sin(2*np.pi*samples) + np.sqrt(var)*np.random.randn(nos, 1)
     m_0 = np.zeros((nob+1, 1))
     p_0 = 0.0001 * np.eye(nob+1)
-    m, p, r = bayesian(X, y, 0.1, m_0, p_0)
+    bys = bayesian(X, y, 0.1, m_0, p_0)
     tests = np.linspace(0, 1, 100)
     Xt = np.zeros((tests.shape[0], nob+1))
     for i, s in enumerate(tests):
         Xt[i, 0] = 1
         for j, c in enumerate(centers):
             Xt[i, 1+j] = np.exp(-0.5 * ((s-c)/width)**2)
-    pred, var = predictive(Xt, 0.1, m, r)
+    pred = predictive(Xt, **bys)
     import pylab
     pylab.plot(samples, y, 'ro')
-    pylab.plot(tests, pred, 'g')
-    pylab.fill_between(tests, pred.flatten() + np.sqrt(var), pred.flatten() - np.sqrt(var), alpha=0.1, color='g')
+    pylab.plot(tests, pred["mean"], 'g')
+    pylab.fill_between(tests, pred["mean"].flatten() + np.sqrt(pred["var"]), 
+            pred["mean"].flatten() - np.sqrt(pred["var"]), alpha=0.1, color='g')
     pylab.plot(tests, np.sin(2*np.pi*tests), 'r', lw=2)
 
 
-def demo_robust(nos, nob):
-    samples = np.random.rand(nos, 1)
+def demo_robust(nos, noo, nob, var=1., k=3, width=0.4):
+    """
+    Use _nos_ many normal samples and
+    _noo_ many outlier samples on the
+    sin function. Use _nob_ many
+    RBF basis functions of _width_.
+    """
+    import random, pylab
+    # Generate 1d inputs
+    samples = np.random.rand(nos+noo, 1)
+
+    # Make design matrix -> RBF's are needed
+    # RBF's are centered equi-distant
     centers = np.linspace(0, 1, nob)
-    width = 4./10
-    X = np.zeros((nos, nob+1))
-    y = np.zeros((nos, 1))
-    var = 1
+    # X is the design matrix
+    X = np.zeros((nos+noo, nob+1))
     for i, s in enumerate(samples):
         X[i, 0] = 1
         for j, c in enumerate(centers):
             X[i, 1+j] = np.exp(-0.5 * ((s-c)/width)**2)
-            w = np.random.gamma(1,1)
-            y[i] = np.sin(2*np.pi*s) + np.sqrt(var/w)*np.random.randn(1)
+
+    # Generate targets
+    y = np.sin(2*np.pi*samples) + np.random.randn(nos+noo, 1)
+    outliers = random.sample(xrange(nos+noo), noo)
+    y[outliers] = np.sin(2*np.pi*samples[outliers]) + (k*k*np.random.rand(noo, 1))*np.sqrt(var)
+    # normalize
+    ym = np.mean(y)
+    ystd = np.std(y)
+    ynormed = (y-ym)/ystd
+
+    # Priors for EM algorithm
+    a = np.ones((nos+noo, 1))
+    b = np.ones((nos+noo, 1))
+    m_0 = np.zeros((nob+1, 1))
+    p_0 = 0.001 * np.eye(nob+1)
+
+    # robust bayesian lr
+    brlr = robust(X, ynormed, m_0, p_0, a, b, var)
+
+    # 'simple' bayesian lr
+    bys = bayesian(X, ynormed, var, m_0, p_0)
+
+    # Test set is [0,1]
     tests = np.linspace(0, 1, 100)
-    import pylab
+    # Design matrix for testset
+    Xt = np.zeros((tests.shape[0], nob+1))
+    for i, s in enumerate(tests):
+        Xt[i, 0] = 1
+        for j, c in enumerate(centers):
+            Xt[i, 1+j] = np.exp(-0.5 * ((s-c)/width)**2)
+
+    # prediction for robust model
+    r_pred = np.dot(Xt, brlr["mean"])
+    
+    # prediction for baysian model
+    b_pred = predictive(Xt, **bys)
+
+    # Plotting business -- boring
     pylab.plot(samples, y, 'ro')
+    # Show outliers in black
+    pylab.plot(samples[outliers], y[outliers], 'ko')
     pylab.plot(tests, np.sin(2*np.pi*tests), 'r', lw=2)
+    pylab.plot(tests, ystd*r_pred + ym, 'g', lw=2)
+    pylab.plot(tests, ystd*b_pred["mean"] + ym, 'b', lw=2)
