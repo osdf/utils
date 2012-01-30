39947b86d0fa76c7f13dc860d00e26a951a2d0a2
osdf <osendorf@gmail.com>
2012-01-30 23:48:03 +0100

    added mia loss and bernoulli KL to losses.py

--- losses.py
+++ losses.py
@@ -6,7 +6,7 @@
 import numpy as np
 
 
-from misc import logsumexp
+from misc import logsumexp, sigmoid
 
 
 def xe(z, targets, predict=False, error=False):
@@ -42,11 +42,31 @@ def ssd(z, targets, weight=0.5, predict=False, error=False):
         return weight*np.sum(err**2)
 
 
-def mia():
+def mia(z, targets, predict=False, error=False):
     """
     Multiple independent attributes.
+
+    Feed model output _z_ through logistic to get
+    bernoulli distributed variables. 
     """
-    pass
+    bern = sigmoid(z)
+    if predict:
+        return bern
+    # loss is binary cross entropy
+    # for every output variable
+    bce =  -( targets*bern.log() + (1-targets)*(1-bern).log() ).sum()
+    if error:
+        return bce, z-targets
+    else:
+        return bce
+    
+
+def bKL(x, y):
+    """
+    Kullback-Leibler divergence between two
+    bernoulli random variables _x_ and _y_.
+    """
+    return x*np.log(x/y) + (1-x)*np.log((1-x)/(1-y))
 
 
 def zero_one(z, targets):
