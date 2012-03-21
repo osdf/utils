fbc8e0a3f34de5a6d848fb676548f2e4cd77a5be
osdf <osendorf@gmail.com>
2012-03-21 22:26:07 +0100

    changed losses to means.

--- losses.py
+++ losses.py
@@ -14,15 +14,14 @@ def xe(z, targets, predict=False, error=False, addon=0):
     """
     if predict:
         return np.argmax(z, axis=1)
-    #
     _xe = z - logsumexp(z, axis=1)
     n, _ = _xe.shape
-    xe = -np.sum(_xe[np.arange(n), targets])
+    xe = -np.mean(_xe[np.arange(n), targets])
     if error:
         err = np.exp(_xe)
         err[np.arange(n), targets] -= 1
         #score + error
-        return xe+addon, err
+        return xe+addon, err/n
     else:
         return xe+addon
 
@@ -33,14 +32,14 @@ def ssd(z, targets, weight=0.5, predict=False, error=False, addon=0):
     """
     if predict:
         return z
-    #
+    n, m = z.shape
     err = z - targets
     if error:
         # rec. error + first deriv
-        return weight*np.sum(err**2)+addon, 2*weight*err
+        return weight*np.mean(err**2)+addon, 2*weight*err/(n*m)
     else:
         # only return reconstruction error 
-        return weight*np.sum(err**2)+addon
+        return weight*np.mean(err**2)+addon
 
 
 def mia(z, targets, predict=False, error=False, addon=0):
@@ -53,12 +52,13 @@ def mia(z, targets, predict=False, error=False, addon=0):
     bern = sigmoid(z)
     if predict:
         return bern
+    n, _ = bern.shape
     # loss is binary cross entropy
     # for every output variable
     bce = -(targets*np.log(bern) + (1-targets)*np.log(1-bern))
-    bce = np.sum(bce)
+    bce = np.mean(np.sum(bce, axis=1))
     if error:
-        return bce+addon, bern-targets
+        return bce+addon, (bern-targets)/n
     else:
         return bce+addon
     
