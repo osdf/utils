36d11faed47b54a051f86dfda4366fe1efc22c1e
osdf <osendorf@gmail.com>
2014-02-17 12:42:43 +0100

    Fixing bugs in synchronous kmeans.

--- osdfutils/mog.py
+++ osdfutils/mog.py
@@ -233,14 +233,17 @@ def skmeans(X, k, epochs, M=None, btsz=100, lr=0.001):
     """
     n, d = X.shape
     if M is None:
-        M = np.random.standard_normal((d, D))
+        M = np.random.standard_normal((d, k))
         Mlength = np.sqrt(np.sum(M**2, axis=0) + 1e-6)
         M /= Mlength
-    print "Starting skmeans."
+    mx = np.zeros((btsz, k))
+    mx_sq = np.zeros((btsz, k))
+    Mlength = np.zeros(k)
     for ep in xrange(epochs):
-        # Cost is not yet computed here -> TODO 
         _cost = 0
         for i in xrange(0, n, btsz):
+            mx[...] = 0.
+            mx_sq[...] = 0.
             _x = X[i:i+btsz]
             sprod = np.dot(_x, M)
             cost = sprod**2
@@ -249,8 +252,9 @@ def skmeans(X, k, epochs, M=None, btsz=100, lr=0.001):
             mx_sq[xrange(_n), idx] = cost[xrange(_n), idx]
             mx_sq_sum = mx_sq.sum(axis=0)
             M += lr*(np.dot(X.T, mx) - mx_sq_sum*M)
-            Mlength = np.sqrt(np.sum(M**2, axis=0) + 1e-6)
+            Mlength[:] = np.sqrt(np.sum(M**2, axis=0) + 1e-6)
             M /= Mlength
+            _cost += np.sum((X - np.dot(mx, M.T))**2)
         print "Epoch: ", ep, "; Cost: ", _cost
     return M, cost, 0
 
