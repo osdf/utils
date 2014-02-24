e9b22b96650205f31e035e538ed438aeee4be870
osdf <osendorf@gmail.com>
2014-02-25 00:06:10 +0100

    zbae.

--- osdfutils/crino.py
+++ osdfutils/crino.py
@@ -20,3 +20,17 @@ def sae():
     synchronous autoencoder.
     """
     pass
+
+
+def zbae():
+    """
+    Zero bias autoencoder.
+    """
+    x = T.matrix('x')
+    W = theano.shared(np.asarray(Winit, dtype=theano.config.floatX,
+        borrow=True, name='W')
+    h = T.dot(x, W)
+    h *= (h>theta)
+    rec = T.sum(x - T.dot(h, W.T), axis=1)
+    cost = T.mean(rec)
+    grads = T.grad(cost, W)
