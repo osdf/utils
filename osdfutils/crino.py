3e90b35536fd3ec00d7fd0df07abc8a369f14637
osdf <osendorf@gmail.com>
2014-02-27 03:20:02 +0100

    ongoing steps.

--- osdfutils/crino.py
+++ osdfutils/crino.py
@@ -13,14 +13,21 @@ def skmeans():
     sprod = T.dot(x, W)
 
     cost = T.sum((X - np.dot(?, W.T))**2)
+    grads = T.grad(cost, W)
 
 
 def sae():
     """
     synchronous autoencoder.
     """
-    pass
-
+    x = T.matrix('x')
+    W = theano.shared(np.asarray(Winit, dtype=theano.config.floatX,
+        borrow=True, name='W')
+    h = T.dot(x, W)
+    sh = h*h
+    rec = T.sum(x - T.dot(sh*h, W.T), axis=1)
+    cost = T.mean(rec)
+    grads = T.grad(cost, W)
 
 def zbae():
     """
