e340aebc03ebc4f3080b6144feb7fd5ced0f5196
osdf <osendorf@gmail.com>
2014-03-04 10:54:47 +0100

    resolved.

--- osdfutils/crino.py
+++ osdfutils/crino.py
@@ -2,6 +2,11 @@
 Some theano offsprings.
 """
 
+import numpy as np
+import theano
+import theano.tensor as T
+from collections import OrderedDict
+
 
 def skmeans():
     """
@@ -39,6 +44,9 @@ def zbae(Winit, activ='TRec', theta=1.):
     x = T.matrix('x')
     W = theano.shared(np.asarray(Winit, dtype=theano.config.floatX),
         borrow=True, name='W')
+    _b = np.zeros((Winit.shape[0],), dtype=theano.config.floatX)
+    b = theano.shared(value=_b, borrow=True)
+
     h = T.dot(x, W)
     if activ is "TRec":
         print "Using TRec as activation"
@@ -46,11 +54,11 @@ def zbae(Winit, activ='TRec', theta=1.):
     else:
         print "Using TLin as activation"
         h = h * ((h*h)> theta)
-    rec = T.sum(x - T.dot(h, W.T), axis=1)
+    rec = T.sum((x - (T.dot(h, W.T)))**2, axis=1)
     cost = T.mean(rec)
-    params = [W]
-    grads = T.grad(cost, W)
-    return params, cost, grads
+    params = (W, )
+    grads = T.grad(cost, params)
+    return params, cost, grads, x
  
 
 def test_zbae(hidden, indim, epochs, lr, momentum, btsz, batches,
@@ -58,17 +66,22 @@ def test_zbae(hidden, indim, epochs, lr, momentum, btsz, batches,
     """
     Test Zero bias AE on rotations.
     """
-    Winit = np.asarray(np.random.standard_normal((indim, hidden)), dtype=theano.config.floatX)
-    params, cost, grads = zbae(Winit, activ=activ, theta=theta)
-    updates = momentum(params, grads)
-    train = theano.function([x], updates=updates, allow_input_downcast=True)
-    # get data
+    print "Generating rotation data ..."
     data = rotations(btsz*batches, indim)
+    
+    print "Building model ..."
+    Winit = np.asarray(0.1 * np.random.standard_normal((data.shape[1], hidden)), dtype=theano.config.floatX)
+    params, cost, grads, x = zbae(Winit, activ=activ, theta=theta)
+    learner = {"lr": lr, "momentum": momentum}
+    updates = momntm(params, grads, **learner)
+    train = theano.function([x], cost, updates=updates, allow_input_downcast=True)
+    # get data
     for epoch in xrange(epochs):
         cost = 0
         for mbi in xrange(batches):
-            cost += train(data[mbi*btsz:(mbi+1)*btsz])
+            cost += btsz*train(data[mbi*btsz:(mbi+1)*btsz])
         print epoch, cost
+    return params
 
 
 def rotations(samples, dims, dist=1., maxangle=30.):
@@ -76,19 +89,18 @@ def rotations(samples, dims, dist=1., maxangle=30.):
     Rotated dots for learning log-polar filters.
     """
     import scipy.ndimage
-    tmps= numpy.random.randn(samples,4*dims*dims)
-    ins = numpy.zeros((samples, dims*dims))
-    outs = numpy.zeros((samples, dims*dims))
+    tmps= np.asarray(np.random.randn(samples,4*dims*dims), dtype=np.float32)
+    seq = np.asarray(np.zeros((samples, 2*dims*dims)), dtype=np.float32)
     for j, img in enumerate(tmps):
-        _angle = numpy.random.vonmises(0.0, dist)/numpy.pi * maxangle
+        _angle = np.random.vonmises(0.0, dist)/np.pi * maxangle
         tmp = scipy.ndimage.interpolation.rotate(img.reshape(2*dims, 2*dims),
             angle=_angle, reshape=False, mode='wrap')
-        outs[j,:] = tmp[dims/2:dims+dims/2,dims/2:dims+dims/2].ravel()
-        _angle = numpy.random.vonmises(0.0, dist)/numpy.pi * maxangle
+        seq[j,:dims*dims] = tmp[dims/2:dims+dims/2,dims/2:dims+dims/2].ravel()
+        _angle = np.random.vonmises(0.0, dist)/np.pi * maxangle
         tmp = scipy.ndimage.interpolation.rotate(img.reshape(2*dims, 2*dims),
             angle=_angle, reshape=False, mode='wrap')
-        ins[j,:] = tmp[dims/2:dims+dims/2,dims/2:dims+dims/2].ravel()
-    return ins, outs
+        seq[j,dims*dims:] = tmp[dims/2:dims+dims/2,dims/2:dims+dims/2].ravel()
+    return seq
 
 
 def shifts(samples, dims, shift=3):
@@ -96,15 +108,15 @@ def shifts(samples, dims, shift=3):
     Produce shifted dots.
     """
     import scipy.ndimage
-    ins = numpy.random.randn(samples,dims*dims)
-    outs = numpy.zeros((samples, dims*dims))
+    ins = np.random.randn(samples,dims*dims)
+    outs = np.zeros((samples, dims*dims))
     for j, img in enumerate(ins):
-        _shift = numpy.random.randint(-shift, shift+1, 2)
+        _shift = np.random.randint(-shift, shift+1, 2)
         outs[j,:] = scipy.ndimage.interpolation.shift(ins[j].reshape(dims, dims), shift=_shift, mode='wrap').ravel()
     return ins, outs
 
 
-def momentum(params, grads, **kwargs):
+def momntm(params, grads, **kwargs):
     """
     Optimizer: SGD with momentum.
     """
