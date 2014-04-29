aea388248eb7101cb9336102d4a96f367599ea5c
osdf <osendorf@gmail.com>
2014-04-29 20:14:46 +0200

    restructure.

--- osdfutils/crino.py
+++ osdfutils/crino.py
@@ -94,18 +94,20 @@ def initweight(shape, variant="normal", **kwargs):
     Init weights.
     """
     if variant is "normal":
-        std = kwargs["std"]
         if "std" in kwargs:
-            std = kwargs["sig"]
+            std = kwargs["std"]
         else:
             std = 0.1
-        return np.asarray(std * np.random.standard_normal(shape), dtype=theano.config.floatX)
+        weights = np.asarray(std * np.random.standard_normal(shape), dtype=theano.config.floatX)
     elif variant is "sparse":
         sparsity = kwargs["sparsity"]
         weights = np.zeroes(shape, dtype=theano.config.floatX)
         for w in weights:
             w[random.sample(xrange(n), sparsity)] = np.random.randn(sparsity)
         weights = weights.T
+    else:
+        assert False, 'Problem in initweight.'
+
     return weights
 
 
@@ -162,30 +164,55 @@ def momntm(params, grads, **kwargs):
     return updates
 
 
-def encoder_dg(x, W1init, W2init, W3init, activ=T.tanh):
+def mlp(config, params, im):
     """
-    An encoder mapping to a multivariate diagonal gaussian.
+    A mlp acting as an encoding or decoding layer --
+    This depends on the loss that is specified
+    in the _config_ dictionary.
     """
-    W1 = theano.shared(np.asarray(W1init, dtype=theano.config.floatX),
-        borrow=True, name='W1_enc')
-    _b1 = np.zeros((W1init.shape[1],), dtype=theano.config.floatX)
-    b1 = theano.shared(value=_b1, borrow=True, name="b1_enc")
+    shapes = config['shapes']
+    activs = config['activs']
 
-    h = activ(T.dot(x, W1) + b1)
+    inpt = im[config['inpt']]
 
-    W2 = theano.shared(np.asarray(W2init, dtype=theano.config.floatX),
-        borrow=True, name='W2_enc')
-    _b2 = np.zeros((W2init.shape[1],), dtype=theano.config.floatX)
-    b2 = theano.shared(value=_b2, borrow=True, name="b2_enc")
+    _tmp_name = config['inpt']
+    for i, (shape, act) in enumerate(zip(shapes, activs)):
+        # fully connected
+        _tmp = initweight(layer[0], variant=config["init"])
+        _tmp_name = "enc_w{0}".format(i)
+        _w = theano.shared(value=_tmp, borrow=True, name=_tmp_name)
+        params.append(_w)
+        # bias
+        _tmp = np.zeros((layer[0][1],), dtype=theano.config.floatX)
+        _tmp_name = "enc_b{0}".format(i)
+        _b = theano.shared(value=_tmp, borrow=True, name=_tmp_name)
+        params.append(_b)
+        inpt = act(T.dot(inpt, _w) + _b)
+        _tmp_name = "enc_layer{0}".format(i)
+        im[_tmp_name] = inpt
 
-    mu = T.dot(h, W2) + b2
+    print "[MLP] building cost."
+    print "[MLP] designated input:", _tmp_name
+    cost_conf = config['cost']
+    cost_conf['inpt'] = _tmp_name
 
-    W3 = theano.shared(np.asarray(W3init, dtype=theano.config.floatX),
-        borrow=True, name='W3_enc')
-    _b3 = np.zeros((W3init.shape[1],), dtype=theano.config.floatX)
-    b3 = theano.shared(value=_b3, borrow=True, name="b3_enc")
+    cost = cost_conf['type']
+    loss = cost(config=cost_conf, params=params, im=im)
 
-    log_var = T.dot(h, W3) + b3
+
+def kl_dg_g(config, params, im):
+    """
+    Kullback-Leibler divergnence between diagonal
+    gaussian and zero/one gaussian.
+    """
+    inpt = im[config['inpt']]
+    dim = config['dim']
+
+    mu = inpt[:, :dim]
+    log_var = inpt[:, dim:]
+
+    im['kl_dg_g_mu'] = mu
+    im['kl_dg_g_log_var'] = log_var
 
     mu_sq = mu * mu
     var = T.exp(log_var)
@@ -195,13 +222,13 @@ def encoder_dg(x, W1init, W2init, W3init, activ=T.tanh):
     gzo = rng.normal(size=mu.shape)
     # Reparameterized latent variable
     z = mu + T.sqrt(var+1e-4)*gzo
-
+    im['z'] = z
+    
     # difference to paper: gradient _descent_, minimize an upper bound
     # -> needs a negative sign
     cost = -(1 + log_var - mu_sq - var)
     cost = T.sum(cost, axis=1)
     cost = 0.5 * T.mean(cost)
-    params = [W1, b1, W2, b2, W3, b3]
     return cost, z, params
 
 
@@ -233,33 +260,84 @@ def decoder_bern(z, t, W1init, W2init, activ=T.tanh):
     return cost, bern, params
 
 
-def vae(encoder, decoder, enc_shapes, dec_shapes):
+def vae(encoder, decoder, platent, tied=False):
     """
     Variational Autoencoder. Provide information on
-    weight shapes via _enc_shapes_ and _dec_shapes_.
-    Autoencoding Variational Bayes, Kingma, Welling. 2014.
+    _encoder_ and _decoder_ in these dictionaries.
+    _platent_ represents the reparametrized latent variable
+    layer. _tied_ indicates wether the first layer of
+    the encoder and the last layer of the decoder are tied.
+
+    See:
+    Autoencoding Variational Bayes by Kingma, Welling. 2014.
     """
+    # journey starts here:
     x = T.matrix('x')
 
-    inits = {}
-    for i, shape in enumerate(enc_shapes):
-        name = "W{0}init".format(i+1)
-        inits[name] = initweight(shape)
+    # collect intermediate expressions, just in case.
+    intermediates = {'inpt': x}
 
-    # build encoder
-    enc_cost, enc_z, enc_params = encoder(x, **inits)
+    # collect parameters
+    params = []
 
-    # build decoder
-    inits = {}
-    for i, shape in enumerate(dec_shapes):
-        name = "W{0}init".format(i+1)
-        inits[name] = initweight(shape)
+    # cost
+    cost = 0
 
-    dec_cost, dec_rec, dec_params = decoder(enc_z, x, **inits)
-    cost = enc_cost + dec_cost
-    params = enc_params + dec_params
-    grads = T.grad(cost, params)
-    return params, cost, grads, x, enc_z, dec_rec
+    enc = encoder['type']
+    pass_over = enc(config=encoder, params=params, im=intermediates)
+
+    plat = platent['type']
+    cost_latent = plat(config=platent, params=params, im=intermediates)
+    cost = cost + cost_latent
+
+    dec = decoder['type']
+    cost_dec = dec(config=decoder, params=params, im=intermediates, passed=pass_over)
+    cost = cost + cost_dec
+
+    return intermediates, cost, params
+
+
+def adadelta(params, grads, **kwargs):
+    """
+    AdaDELTA, by Matthew Zeiler.
+    """
+    eps = 10e-8
+    print "OPTIMIZER: AdaDELTA"
+    lr = kwargs['lr']
+    decay = kwargs['decay']
+
+    # Average Root Mean Squared (arms) Gradients
+    arms_grads = []
+    # Average Root Mean Squared (arms) Updates
+    arms_upds = []
+    for p in params:
+        arms_grad_i = theano.shared(np.zeros(p.get_value(borrow=True).shape,
+            dtype=theano.config.floatX))
+        arms_grads.append(arms_grad_i)
+        #
+        arms_upds_i = theano.shared(np.zeros(p.get_value(borrow=True).shape,
+            dtype=theano.config.floatX))
+        arms_upds.append(arms_upds_i)
+
+    updates = OrderedDict()
+    for grad_i, arms_grad_i in zip(grads, arms_grads):
+        updates[arms_grad_i] = decay*arms_grad_i + (1-decay)*grad_i*grad_i
+
+    for param_i, grad_i, arms_grad_i, arms_upds_i in zip(params, grads, arms_grads, arms_upds):
+        delta_i = T.sqrt(arms_upds_i + eps)/T.sqrt(updates[arms_grad_i] + eps) * lr * grad_i
+        updates[arms_upds_i] = decay*arms_upds_i + (1-decay)*delta_i*delta_i
+        
+        if param_i.name in kwargs:
+            up = param_i - delta_i
+            wl = T.sqrt(T.sum(T.square(up), axis=kwargs[param_i.name]) + 1e-8)
+            if kwargs[param_i.name] == 0:
+                updates[param_i] = up/wl
+            else:
+                updates[param_i] = up/wl.dimshuffle(0, 'x')
+            print "Normalized {0} along axis {1}".format(param_i.name, kwargs[param_i.name])
+        else:
+            updates[param_i] = param_i - delta_i
+    return updates
 
 
 def test_vae(enc_hidden=500, enc_out=2, dec_hidden=500,
