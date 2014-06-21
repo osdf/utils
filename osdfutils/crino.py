Sign mistake in nll.

osdfutils/crino.py
@@ -956,7 +956,7 @@ def dg_nll(config, params, im):
     # difference to paper: gradient _descent_, minimize upper bound
     # -> needs a negative sign
     cost = ((pred - t)**2)/var 
-    cost = 0.5*T.sum(cost, axis=1) - dim/2*T.log(2*np.pi) - T.sum(var, axis=1) 
+    cost = 0.5*T.sum(cost, axis=1) + dim/2*T.log(2*np.pi) + T.sum(log_var, axis=1) 
     cost = T.mean(cost)
     im['fg_nll'] = cost
     im['cost'] = im['cost'] + cost