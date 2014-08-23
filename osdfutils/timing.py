f89b3f29805831b8448bbaf14a4ef82c3402c843
osdf <osendorf@gmail.com>
2014-08-23 22:22:20 +0200

    fix.

--- osdfutils/timing.py
+++ osdfutils/timing.py
@@ -4,30 +4,30 @@ from http://stackoverflow.com/questions/1557571/how-to-get-time-of-a-python-prog
 
 
 import atexit
-from time import clock
+import time
 
-def secondsToStr(t):
-    return "%d:%02d:%02d.%03d" % \
-        reduce(lambda ll,b : divmod(ll[0],b) + ll[1:],
-            [(t*1000,),1000,60,60])
 
-line = "="*40
-def log(s, elapsed=None):
-    print line
-    print secondsToStr(clock()), '-', s
+def log(s, elapsed=None, start=None):
+    print s
     if elapsed:
         print "Elapsed time:", elapsed
-    print line
+    if start:
+        tm = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(start))
+        print tm
     print
 
+
 def endlog():
-    end = clock()
-    elapsed = end-start
-    log("End Program", secondsToStr(elapsed))
+    end = time.time()
+    elapsed = end - start
+    print
+    log("End Program", elapsed=elapsed)
+
 
 def now():
-    return secondsToStr(clock())
+    return time.time()
 
-start = clock()
+
+start = time.time()
 atexit.register(endlog)
-log("Start Program")
+log("Start Program", elapsed=None, start=start)
